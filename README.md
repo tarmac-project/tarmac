3d84c86eb7ff7a26d88ba4420518da5bf16301f5
Benjamin Cane <ben@bencane.com>
2021-06-14 07:55:18 -0700

    Adding Callbacks server and making a better example file

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ Using Web Assembly (WASM), Tarmac users can write their logic in many different 
 
 Like other FaaS services, it's easy to use Tarmac to create either Functions or full Microservices. However, unlike FaaS platforms, as Web Assembly (WASM) & Web Assembly System Interface (WASI) matures, Tarmac will provide users with much more than an easy way to run a function inside a Docker container.
 
-By leveraging Web Assembly System Interface (WASI), Tarmac creates an isolated environment for running functions. But unlike FaaS platforms, Tarmac users will be able to import Tarmac functions (still a work in progress, experimenting with gRPC currently).
+By leveraging Web Assembly System Interface (WASI), Tarmac creates an isolated environment for running functions. But unlike FaaS platforms, Tarmac users will be able to import Tarmac functions (still a work in progress).
 
 Like any other microservice framework, the goal is Tarmac will handle the complexities of Database Connections, Caching, Metrics, and Dynamic Configuration. Users can focus purely on the function logic and writing it in their favorite programming language.
 
@@ -25,41 +25,39 @@ Of course, Contributions are also welcome.
 
 ## Getting Started with Tarmac
 
-At the moment, Tarmac can run WASI-generated WASM code. However, the serverless function code must follow a pre-defined function signature with Tarmac executing an `HTTPHandler()` function.
+At the moment, Tarmac can run WASI-generated WASM code. However, the serverless function code must follow a pre-defined function signature with Tarmac executing a defined `request:handler` function.
 
-To understand this better, take a look at our simple example written in Go (found in [example/go](example/go)).
+To understand this better, take a look at our simple example written in Rust (found in [example/](example/)).
 
-```golang
-package main
+```rust
+extern crate wapc_guest as guest;
 
-import (
-        "encoding/base64"
-        "fmt"
-        "os"
-)
+use guest::prelude::*;
 
-//export HTTPHandler
-func HTTPHandler() int {
-        d, err := base64.StdEncoding.DecodeString(os.Getenv("HTTP_PAYLOAD"))
-        if err != nil {
-                fmt.Fprintf(os.Stderr, "Invalid Payload")
-                return 400
-        }
-        if os.Getenv("HTTP_METHOD") == "POST" || os.Getenv("HTTP_METHOD") == "PUT" {
-                fmt.Fprintf(os.Stdout, "%s", d)
-        }
-        return 200
+fn main() {}
+
+#[no_mangle]
+pub extern "C" fn wapc_init() {
+  // Register our hello world function for Tarmac to call
+  register_function("request:handler", hello_world);
 }
 
-func main() {}
+fn hello_world(msg: &[u8]) -> CallResult {
+    // Callback to Tarmac to Log the incoming payload
+    let _res = host_call("tarmac", "logging", "Debug", &msg.to_vec())?;
+
+    // Return the provided payload back to Tarmac
+    Ok(msg.to_vec())
+}
 ```
 
-As we can see from the above code, Tarmac passes the HTTP Context and Payload through Environment Variables (at the moment). The Payload is Base64 encoded but otherwise untouched.
+Tarmac passes the HTTP Context and Payload to the WASM guest via the incoming `msg`. The `msg` is a JSON which contains Headers and a Payload which is Base64 encoded but otherwise untouched.
 
 To compile the example above, run:
 
 ```shell
-$ tinygo build -o tarmac_module.wasm -target wasi ./main.go
+$ cd example/hello
+$ make build
 ```
 
 Once compiled, users can run Tarmac using the following command:
---
+++ callbacks/callbacks.go
@@ -0,0 +1,138 @@
+/*
+Package callbacks provides a Host Callback router for wapc-go servers.
+
+With this package, users can register Callback functions with the specified namespace and function name. Users can then set this router's Callback method in the waPC server.
+
+When the waPC server calls the callback function, the provided namespace and function name will be used as a lookup to execute the pre-defined host callback.
+
+  router := callbacks.New()
+  router.RegisterCallback("database:kv", "Get", myFunc)
+  router.RegisterCallback("database:kv", "Set", myFunc2)
+  router.RegisterCallback("database:kv", "Delete", myFunc3)
+
+  module, err := wapc.New(someCode, router.Callback)
+  if err != nil {
+    // do stuff
+  }
+
+*/
+package callbacks
+
+import (
+	"context"
+	"fmt"
+	"sync"
+)
+
+var (
+	ErrNotFound = fmt.Errorf("callback not found")
+	ErrCanceled = fmt.Errorf("callback context has expired")
+)
+
+// Router is a callback router, which provides a Callback function that users can register with wapc-go. This callback function will
+// determine which registered callback to execute based on the namespace and function name when invoked.
+type Router struct {
+	sync.RWMutex
+
+	// callbacks stores registered callbacks for lookups when the router is called.
+	callbacks map[string]*Callback
+
+	// preFunc holds a user-defined function called as part of the primary Callback method but before the user-specified callback.
+	// This preFunc can act as middleware enabling users to restrict access to specific callbacks or perform standard functions with all callbacks.
+	preFunc func(string, string, []byte) ([]byte, error)
+}
+
+// Config will configure the Callbacks router and allows users to specify items such as the PreFunc or any router level configurations.
+type Config struct {
+	// PreFunc holds a user-defined function called as part of the primary Callback method but before the user-specified callback.
+	// This PreFunc can act as middleware enabling users to restrict access to specific callbacks or perform standard functions with all callbacks.
+	PreFunc func(string, string, []byte) ([]byte, error)
+}
+
+// Callback is a type that holds the details and function used for callback execution. This type is primarily used internally but
+// also returned from some public methods.
+type Callback struct {
+	// Namespace is the common namespace for this callback, and an example could be "database" or for a specific type of
+	// database, "database:kv".
+	Namespace string
+
+	// Operation is the function within the namespace to call. For example, a "database:kv" namespace may have a "Get" function.
+	Operation string
+
+	// Func is the user-provided callback function. This function will execute when the router receives a call with the
+	// specified Namespace and Operation key.
+	Func func([]byte) ([]byte, error)
+}
+
+// New will return a Router instance that users can use to register callbacks and provide a generic host callback function.
+func New(cfg Config) *Router {
+	r := &Router{
+		preFunc: cfg.PreFunc,
+	}
+	r.callbacks = make(map[string]*Callback)
+	return r
+}
+
+// Lookup will fetch the Callback requested from the internal map storage.
+func (r *Router) Lookup(key string) (*Callback, error) {
+	r.RLock()
+	defer r.RUnlock()
+	if c, ok := r.callbacks[key]; ok {
+		return c, nil
+	}
+	return &Callback{}, ErrNotFound
+}
+
+// Callback is the public callback method, users can register this method as part of a waPC-go module, and when called,
+// it will determine if the received host call has a registered callback or not. If yes, this method will execute the
+// registered Callback; if not, it will return an error indicating the callback method not found.
+func (r *Router) Callback(ctx context.Context, binding, namespace, op string, data []byte) ([]byte, error) {
+	if namespace == "" || op == "" {
+		return []byte(""), fmt.Errorf("namespace and op cannot be nil")
+	}
+
+	// Lookup registered Callback
+	key := fmt.Sprintf("%s:%s", namespace, op)
+	c, err := r.Lookup(key)
+	if err != nil {
+		return []byte(""), err
+	}
+
+	// If callback context is canceled return error
+	if ctx.Err() != nil {
+		return []byte(""), ErrCanceled
+	}
+
+	// Call the user-defined PreFunc returning any errors to the caller.
+	if r.preFunc != nil {
+		b, err := r.preFunc(namespace, op, data)
+		if err != nil {
+			return b, err
+		}
+	}
+
+	// Run the callback returning to user
+	if c.Func != nil {
+		return c.Func(data)
+	}
+	return []byte(""), fmt.Errorf("unable to execute callback function, function is nil")
+}
+
+// RegisterCallback will add the provided function into the internal map store of Callbacks, which will
+// enable the Callback method to find and execute the supplied function when appropriate.
+func (r *Router) RegisterCallback(namespace, op string, f func([]byte) ([]byte, error)) {
+	r.Lock()
+	defer r.Unlock()
+	r.callbacks[fmt.Sprintf("%s:%s", namespace, op)] = &Callback{
+		Namespace: namespace,
+		Operation: op,
+		Func:      f,
+	}
+}
+
+// DelCallback will remove any Callback functions saved for the specified namespace and operation.
+func (r *Router) DelCallback(namespace, op string) {
+	r.Lock()
+	defer r.Unlock()
+	delete(r.callbacks, fmt.Sprintf("%s:%s", namespace, op))
+}
