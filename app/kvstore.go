b54b0078d22485b6672481c636bde37994367d4a
Benjamin Cane <ben@bencane.com>
2021-06-17 08:06:35 -0700

    Adding comments to kvstore code

--- app/kvstore.go
+++ app/kvstore.go
@@ -7,13 +7,21 @@ import (
 	"github.com/pquerna/ffjson/ffjson"
 )
 
+// kvStore provides access to Host Callbacks that interact with the key:value datastores within Tarmac. The callbacks
+// within kvStore provided all of the logic and error handlings of accessing and interacting with a key:value
+// database. Users will send the specified JSON request and receive an appropriate JSON response.
 type kvStore struct{}
 
+// Get will fetch the stored data using the key specified within the incoming JSON. Logging, error handling, and
+// base64 encoding of data are all handled via this function. Note, this function expects the KVStoreGetRequest
+// JSON type as input and will return a KVStoreGetResponse JSON.
 func (k *kvStore) Get(b []byte) ([]byte, error) {
+	// Start Response Message assuming everything is good
 	r := tarmac.KVStoreGetResponse{}
 	r.Status.Code = 200
 	r.Status.Status = "OK"
 
+	// Parse incoming Request
 	var rq tarmac.KVStoreGet
 	err := ffjson.Unmarshal(b, &rq)
 	if err != nil {
@@ -21,16 +29,20 @@ func (k *kvStore) Get(b []byte) ([]byte, error) {
 		r.Status.Status = "Error Parsing Input"
 	}
 
-	// Fetch data from KVStore
-	data, err := kv.Get(rq.Key)
-	if err != nil {
-		r.Status.Code = 404
-		r.Status.Status = fmt.Sprintf("Unable to fetch key %s - %s", rq.Key, err)
+	// Fetch data from KVStore if we do not have any other errors
+	if r.Status.Code == 200 {
+		data, err := kv.Get(rq.Key)
+		if err != nil {
+			r.Status.Code = 404
+			r.Status.Status = fmt.Sprintf("Unable to fetch key %s - %s", rq.Key, err)
+		}
+
+		// Encode Fetched Data to store within JSON
+		r.Data = base64.StdEncoding.EncodeToString(data)
+
 	}
 
-	// Encode Fetched Data
-	r.Data = base64.StdEncoding.EncodeToString(data)
-
+	// Marshal a resposne JSON to return to caller
 	rsp, err := ffjson.Marshal(r)
 	if err != nil {
 		log.Errorf("Unable to marshal kvstore:get response - %s", err)
@@ -43,11 +55,16 @@ func (k *kvStore) Get(b []byte) ([]byte, error) {
 	return rsp, fmt.Errorf("%s", r.Status.Status)
 }
 
+// Set will store data within the key:value datastore using the key specified within the incoming JSON. Logging, error
+// handling, and base64 decoding of data are all handled via this function. Note, this function expects the
+// KVStoreSetRequest JSON type as input and will return a KVStoreSetResponse JSON.
 func (k *kvStore) Set(b []byte) ([]byte, error) {
+	// Start Response Message assuming everything is good
 	r := tarmac.KVStoreSetResponse{}
 	r.Status.Code = 200
 	r.Status.Status = "OK"
 
+	// Parse incoming Request
 	var rq tarmac.KVStoreSet
 	err := ffjson.Unmarshal(b, &rq)
 	if err != nil {
@@ -62,7 +79,7 @@ func (k *kvStore) Set(b []byte) ([]byte, error) {
 		r.Status.Status = fmt.Sprintf("Unable to decode data - %s", err)
 	}
 
-	// Store data in KVStore
+	// Store data in KVStore if we do not have any other errors
 	if r.Status.Code == 200 {
 		err = kv.Set(rq.Key, data)
 		if err != nil {
@@ -71,6 +88,7 @@ func (k *kvStore) Set(b []byte) ([]byte, error) {
 		}
 	}
 
+	// Marshal a resposne JSON to return to caller
 	rsp, err := ffjson.Marshal(r)
 	if err != nil {
 		log.Errorf("Unable to marshal kvstore:set response - %s", err)
@@ -83,11 +101,16 @@ func (k *kvStore) Set(b []byte) ([]byte, error) {
 	return rsp, fmt.Errorf("%s", r.Status.Status)
 }
 
+// Delete will remove the key and data stored within the key:value datastore using the key specified within the incoming
+// JSON. Logging and error handling are all handled via this callback function. Note, this function expects the
+// KVStoreDeleteRequest JSON type as input and will return a KVStoreDeleteResponse JSON.
 func (k *kvStore) Delete(b []byte) ([]byte, error) {
+	// Start Response Message assuming everything is good
 	r := tarmac.KVStoreDeleteResponse{}
 	r.Status.Code = 200
 	r.Status.Status = "OK"
 
+	// Parse incoming Request
 	var rq tarmac.KVStoreDelete
 	err := ffjson.Unmarshal(b, &rq)
 	if err != nil {
@@ -95,12 +118,16 @@ func (k *kvStore) Delete(b []byte) ([]byte, error) {
 		r.Status.Status = "Error Parsing Input"
 	}
 
-	err = kv.Delete(rq.Key)
-	if err != nil {
-		r.Status.Code = 404
-		r.Status.Status = fmt.Sprintf("Unable to delete key %s - %s", rq.Key, err)
+	// Delete data in KVStore if we do not have any other errors
+	if r.Status.Code == 200 {
+		err = kv.Delete(rq.Key)
+		if err != nil {
+			r.Status.Code = 404
+			r.Status.Status = fmt.Sprintf("Unable to delete key %s - %s", rq.Key, err)
+		}
 	}
 
+	// Marshal a response JSON to return to caller
 	rsp, err := ffjson.Marshal(r)
 	if err != nil {
 		log.Errorf("Unable to marshal kvstore:delete response - %s", err)
