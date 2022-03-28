d633eb0cdbc1559bd571c053ebf0e9b1f36af772
Benjamin Cane <ben@bencane.com>
2022-03-27 18:38:42 -0700

    Fixing Go Tac Example

--- example/tac/go/main.go
+++ example/tac/go/main.go
@@ -3,9 +3,7 @@
 package main
 
 import (
-	"encoding/base64"
 	"fmt"
-	"github.com/valyala/fastjson"
 	wapc "github.com/wapc/wapc-guest-tinygo"
 )
 
@@ -14,51 +12,38 @@ func main() {
 	// appropriate method as shown below.
 	wapc.RegisterFunctions(wapc.Functions{
 		// Register a GET request handler
-		"http:GET": NoHandler,
+		"GET": NoHandler,
 		// Register a POST request handler
-		"http:POST": Handler,
+		"POST": Handler,
 		// Register a PUT request handler
-		"http:PUT": Handler,
+		"PUT": Handler,
 		// Register a DELETE request handler
-		"http:DELETE": NoHandler,
+		"DELETE": NoHandler,
 	})
 }
 
 // NoHandler is a custom Tarmac Handler function that will return a tarmac.ServerResponse JSON that denies
 // the client request.
 func NoHandler(payload []byte) ([]byte, error) {
-	return []byte(`{"status":{"code":503,"status":"Not Implemented"}}`), nil
+	return []byte(""), fmt.Errorf("Not Implemented")
 }
 
 // Handler is the custom Tarmac Handler function that will receive a tarmac.ServerRequest JSON payload and
 // must return a tarmac.ServerResponse JSON payload along with a nil error.
 func Handler(payload []byte) ([]byte, error) {
-	// Parse the JSON request
-	rq, err := fastjson.ParseBytes(payload)
-	if err != nil {
-		return []byte(fmt.Sprintf(`{"status":{"code":500,"status":"Failed to call parse json - %s"}}`, err)), nil
-	}
-
-	// Decode the payload
-	s, err := base64.StdEncoding.DecodeString(string(rq.GetStringBytes("payload")))
-	if err != nil {
-		return []byte(fmt.Sprintf(`{"status":{"code":500,"status":"Failed to perform base64 decode - %s"}}`, err)), nil
-	}
-	b := []byte(s)
-
 	// Perform a host callback to log the incoming request
-	_, err = wapc.HostCall("tarmac", "logger", "trace", []byte(fmt.Sprintf("Reversing Payload: %s", s)))
+	_, err := wapc.HostCall("tarmac", "logger", "trace", []byte(fmt.Sprintf("Reversing Payload: %s", payload)))
 	if err != nil {
-		return []byte(fmt.Sprintf(`{"status":{"code":500,"status":"Failed to call host callback - %s"}}`, err)), nil
+		return []byte(""), fmt.Errorf("Unable to call callback - %s", err)
 	}
 
 	// Flip it and reverse
-	if len(b) > 0 {
-		for i, n := 0, len(b)-1; i < n; i, n = i+1, n-1 {
-			b[i], b[n] = b[n], b[i]
+	if len(payload) > 0 {
+		for i, n := 0, len(payload)-1; i < n; i, n = i+1, n-1 {
+			payload[i], payload[n] = payload[n], payload[i]
 		}
 	}
 
 	// Return the payload via a ServerResponse JSON
-	return []byte(fmt.Sprintf(`{"payload":"%s","status":{"code":200,"status":"Success"}}`, base64.StdEncoding.EncodeToString(b))), nil
+	return payload, nil
 }
