9e701e55ac2e7a9779f158c0ca16e499c2f44698
Benjamin Cane <ben@bencane.com>
2022-07-25 17:54:57 -0700

    adding tlsconfig package

---
+++ pkg/tlsconfig/tlsconfig.go
@@ -0,0 +1,102 @@
+/*
+Package tlsconfig is a helper package used to create TLS configuration that adheres to best practices.
+
+This package aims to write less repetitive code when creating TLS configurations. Opening certs, bundling certificate
+ authorities, configuring ciphers, etc. Just use this package to save yourself some headaches.
+*/
+package tlsconfig
+
+import (
+	"crypto/tls"
+  "crypto/x509"
+  "fmt"
+  "io/ioutil"
+)
+
+// Config is used to create an instance of the configuration helper. It holds the basic TLS configuration for
+// repeated generation.
+type Config struct {
+	config tls.Config
+}
+
+// New will create a new config instance with basic TLS best practices pre-defined.
+func New() Config {
+	c := Config{
+		config: tls.Config{
+			// Restrict to use TLS 1.2 as the minimum TLS versions
+			MinVersion: tls.VersionTLS12,
+			// Restrict Cipher Suites
+			CipherSuites: []uint16{
+				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_AES_128_GCM_SHA256,
+				tls.TLS_AES_256_GCM_SHA384,
+			},
+		},
+	}
+	return c
+}
+
+// CertsFromFile will read the certificate and key file and create an X509 KeyPair loaded as 
+// Certificates. The files must contain PEM encoded data. The certificate file may contain 
+// intermediate certificates following the leaf certificate to form a certificate chain.
+func (c *Config) CertsFromFile(cert, key string) error {
+	if cert == "" || key == "" {
+		return fmt.Errorf("cert and key cannot be empty")
+	}
+
+	pair, err := tls.LoadX509KeyPair(cert, key)
+	if err != nil {
+		return fmt.Errorf("unable to load keypair - %s", err)
+	}
+
+	c.config.Certificates = append(c.config.Certificates, pair)
+	return nil
+}
+
+// CAFromFile will read the PEM encoded certificate authority file and register the 
+// certificate as an authority for Client Authentication. This function is for m-TLS 
+// configuration at the server level. By default, this function sets Client 
+// Authentication to Require and Verify the Certificate.
+func (c *Config) CAFromFile(ca string) error {
+	c.config.ClientAuth = tls.RequireAndVerifyClientCert
+
+  if ca == "" {
+    return fmt.Errorf("ca cannot be empty")
+  }
+
+	b, err := ioutil.ReadFile(ca)
+	if err != nil {
+		return fmt.Errorf("unable to read ca file - %s", err)
+	}
+
+	pool := x509.NewCertPool()
+	if !pool.AppendCertsFromPEM(b) {
+		return fmt.Errorf("unable to load ca certificate - %s", err)
+	}
+
+	c.config.ClientCAs = pool
+	return nil
+}
+
+// IgnoreClientCert will set client certificate authentication to verify the certificate 
+// only if provided. Otherwise, if no certificate is provided, the client will still be allowed.
+func (c *Config) IgnoreClientCert() {
+	c.config.ClientAuth = tls.VerifyClientCertIfGiven
+	return
+}
+
+// IgnoreHostValidation will turn off the hostname validation of certificates. This 
+// setting is dangerous and should only be used in testing.
+func (c *Config) IgnoreHostValidation() {
+	c.config.InsecureSkipVerify = true
+	return
+}
+
+// Generate will create a TLS configuration type based on the defaults and settings called. 
+// Users can run this multiple times to produce the same configuration.
+func (c *Config) Generate() *tls.Config {
+	return c.config.Clone()
+}
---
+++ pkg/tlsconfig/tlsconfig_test.go
@@ -0,0 +1,141 @@
+package tlsconfig
+
+import (
+	"crypto/tls"
+	"github.com/madflojo/testcerts"
+	"os"
+	"testing"
+)
+
+type TestCase struct {
+	name          string
+	ca            string
+	caPass        bool
+	cert          string
+	certsPass     bool
+	generateCerts bool
+	key           string
+}
+
+func TestTLSConfig(t *testing.T) {
+	var tc []TestCase
+
+	tc = append(tc, TestCase{
+		name:          "Happy Path",
+		ca:            "/tmp/cert",
+		caPass:        true,
+		cert:          "/tmp/cert",
+		certsPass:     true,
+		generateCerts: true,
+		key:           "/tmp/key",
+	})
+
+	tc = append(tc, TestCase{
+		name:          "Empty CA",
+		ca:            "",
+		caPass:        false,
+		cert:          "/tmp/cert",
+		certsPass:     true,
+		generateCerts: true,
+		key:           "/tmp/key",
+	})
+
+	tc = append(tc, TestCase{
+		name:          "Empty Cert Values",
+		ca:            "",
+		caPass:        false,
+		cert:          "",
+		certsPass:     false,
+		generateCerts: false,
+		key:           "",
+	})
+
+	tc = append(tc, TestCase{
+		name:          "No CA",
+		ca:            "/tmp/nope",
+		caPass:        false,
+		cert:          "/tmp/cert",
+		certsPass:     true,
+		generateCerts: true,
+		key:           "/tmp/key",
+	})
+
+	tc = append(tc, TestCase{
+		name:          "No Certs",
+		ca:            "/tmp/cert",
+		caPass:        false,
+		cert:          "/tmp/cert",
+		certsPass:     false,
+		generateCerts: false,
+		key:           "/tmp/key",
+	})
+
+	for _, c := range tc {
+		t.Run(c.name, func(t *testing.T) {
+			b := New()
+
+			t.Run("Base Config", func(t *testing.T) {
+				bc := b.Generate()
+
+				if bc.MinVersion != tls.VersionTLS12 {
+					t.Errorf("Unexpected TLS version set as minimum")
+				}
+
+				if bc.InsecureSkipVerify {
+					t.Errorf("Unexpected value for Host verification")
+				}
+			})
+
+			t.Run("WithCerts", func(t *testing.T) {
+				// Generate Certs
+				if c.generateCerts {
+					err := testcerts.GenerateCertsToFile(c.cert, c.key)
+					if err != nil {
+						t.Fatalf("Could not generate certs for test - %s", err)
+					}
+					defer os.Remove(c.cert)
+					defer os.Remove(c.key)
+				}
+
+				err := b.CertsFromFile(c.cert, c.key)
+				if err != nil && c.certsPass {
+					t.Fatalf("Unable to generate with certs - %s", err)
+				}
+				if !c.certsPass && err == nil {
+					t.Fatalf("Unexpected success Loading certs from a file")
+				}
+
+				t.Run("WithCA", func(t *testing.T) {
+					err := b.CAFromFile(c.ca)
+					if err != nil && c.caPass {
+						t.Fatalf("Unable to generate with CA - %s", err)
+					}
+					if !c.caPass && err == nil {
+						t.Fatalf("Unexpected success Loading ca from file")
+					}
+
+					bc := b.Generate()
+					if bc.ClientAuth != tls.RequireAndVerifyClientCert {
+						t.Errorf("Unexpected default client auth value")
+					}
+				})
+			})
+
+			t.Run("IgnoreHostVerification", func(t *testing.T) {
+				b.IgnoreHostValidation()
+				bc := b.Generate()
+				if !bc.InsecureSkipVerify {
+					t.Errorf("Unexpected value for Host verification")
+				}
+			})
+
+			t.Run("IgnoreClientCert", func(t *testing.T) {
+				b.IgnoreClientCert()
+				bc := b.Generate()
+				if bc.ClientAuth != tls.VerifyClientCertIfGiven {
+					t.Errorf("Unexpected value for Client Auth")
+				}
+			})
+		})
+	}
+}
