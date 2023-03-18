aaec6360a207ebd8592fb2686b2b040da5080ce0
Benjamin Cane <ben@bencane.com>
2023-03-18 07:16:51 -0700

    Adding metrics

---
+++ metrics.go
@@ -0,0 +1,85 @@
+package tarmac
+
+import "fmt"
+
+// Metrics provides an interface to the host metrics
+type Metrics struct {
+	namespace string
+	hostCall  func(string, string, string, []byte) ([]byte, error)
+}
+
+// newMetrics creates a metrics interface for creating Counters, Histograms, and Gauges.
+func newMetrics(cfg Config) *Metrics {
+	return &Metrics{namespace: cfg.Namespace, hostCall: cfg.hostCall}
+}
+
+// Counter provides an Counter metric which can be incremented.
+type Counter struct {
+	name      string
+	namespace string
+	hostCall  func(string, string, string, []byte) ([]byte, error)
+}
+
+// newCounter creates a new Counter metric with the given name.
+func (m *Metrics) newCounter(name string) (*Counter, error) {
+	if name == "" {
+		return &Counter{}, fmt.Errorf("name cannot be empty")
+	}
+
+	c := &Counter{name: name, namespace: m.namespace, hostCall: m.hostCall}
+	return c, nil
+}
+
+// Inc increments the counter by 1.
+func (c *Counter) Inc() {
+	_, _ = c.hostCall(c.namespace, "metrics", "counter", []byte(fmt.Sprintf(`{"name":"%s"}`, c.name)))
+}
+
+// Histogram provides a Histogram metric which can observe values.
+type Histogram struct {
+	name      string
+	namespace string
+	hostCall  func(string, string, string, []byte) ([]byte, error)
+}
+
+// newHistogram creates a new Histogram metric with the given name.
+func (m *Metrics) newHistogram(name string) (*Histogram, error) {
+	if name == "" {
+		return &Histogram{}, fmt.Errorf("name cannot be empty")
+	}
+
+	h := &Histogram{name: name, namespace: m.namespace, hostCall: m.hostCall}
+	return h, nil
+}
+
+// Observe adds a value to the histogram.
+func (h *Histogram) Observe(f float64) {
+	_, _ = h.hostCall(h.namespace, "metrics", "histogram", []byte(fmt.Sprintf(`{"name":"%s","value": %f}`, h.name, f)))
+}
+
+// Gauge provides a Gauge metric which can be incremented and decremented.
+type Gauge struct {
+	name      string
+	namespace string
+	hostCall  func(string, string, string, []byte) ([]byte, error)
+}
+
+// newGauge creates a new Gauge metric with the given name.
+func (m *Metrics) newGauge(name string) (*Gauge, error) {
+	if name == "" {
+		return &Gauge{}, fmt.Errorf("name cannot be empty")
+	}
+
+	g := &Gauge{name: name, namespace: m.namespace, hostCall: m.hostCall}
+	return g, nil
+}
+
+// Inc increments the gauge by 1.
+func (g *Gauge) Inc() {
+	_, _ = g.hostCall(g.namespace, "metrics", "gauge", []byte(fmt.Sprintf(`{"name":"%s","action": "inc"}`, g.name)))
+}
+
+// Dec decrements the gauge by 1.
+func (g *Gauge) Dec() {
+	_, _ = g.hostCall(g.namespace, "metrics", "gauge", []byte(fmt.Sprintf(`{"name":"%s","action": "dec"}`, g.name)))
+}
