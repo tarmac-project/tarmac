50a32d798dde49135cacb58ad12bf816651136e2
Benjamin Cane <ben@bencane.com>
2023-03-19 11:24:49 -0700

    Restructuring Logger

--- pkg/sdk/sdk.go
+++ pkg/sdk/sdk.go
@@ -4,11 +4,29 @@ Package sdk is a client package for WASM functions running within a Tarmac serve
 This package provides user-friendly functions that wrap the Web Assembly Procedure Call (waPC) based functions of
 Tarmac. Guest WASM functions running inside Tarmac can use this library to call back the Tarmac host and perform
 host-level actions such as storing data within the database, logging specific data, or looking up configurations.
+
+	import "github.com/madflojo/tarmac/pkg/sdk"
+	
+	var tarmac *Tarmac
+	
+	func main() {
+		tarmac, err := sdk.New(sdk.Config{Namespace: "my-service", Handler: Handler})
+		if err != nil {
+			// do something
+		}
+	}
+	
+	func Handler(payload []byte) ([]byte, error) {
+		tarmac.Logger.Info("This is a log message")
+		return []byte("Hello World"), nil
+	}
+
 */
 package sdk
 
 import (
 	"fmt"
+	"github.com/madflojo/tarmac/pkg/sdk/logger"
 	wapc "github.com/wapc/wapc-guest-tinygo"
 )
 
@@ -22,11 +40,7 @@ type Tarmac struct {
 	// Handler registers the user function to execute as part of the Tarmac Function.
 	handler func([]byte) ([]byte, error)
 
-	// Logger provides an interface to the standard logger
-	Logger *Logger
-
-	// KV provides an interface to the KVStores within Tarmac
-	KV *KV
+	Logger *logger.Logger
 }
 
 // Config provides users with the ability to specify namespaces, function handlers and other key information required to execute the
@@ -63,11 +77,13 @@ func New(cfg Config) (*Tarmac, error) {
 	// Set hostCall function for internal callbacks
 	cfg.hostCall = wapc.HostCall
 
-	// Initialize a Logger
-	t.Logger = newLogger(cfg)
+	var err error
 
-	// Initialize a KVStore
-	t.KV = newKVStore(cfg)
+	// Initialize a Logger instance
+	t.Logger, err = logger.NewLogger(logger.Config{Namespace: cfg.Namespace, HostCall: cfg.hostCall})
+	if err != nil {
+		return t, fmt.Errorf("error while initializing logger - %s", err)
+	}
 
 	return t, nil
 }
