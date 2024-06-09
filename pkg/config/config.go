03b1328a0246f6fda66619394f2280fffb23075d
Benjamin Cane <ben@bencane.com>
2024-06-09 08:40:12 -0700

    Adding Config Validation

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -46,7 +46,13 @@ type Function struct {
 
 // Route defines available routes for the service.
 type Route struct {
-	// Type defines the protocol used for the route, examples are "http", "nats", "scheduled_task", etc.
+	// Type defines the Route type for the function.
+	//
+	// Valid types are:
+	// - http - HTTP based routes
+	// - function - Function to Function calls
+	// - scheduled_task - Scheduled function calls
+	// - init - Initialization functions
 	Type string `json:"type"`
 
 	// Path defines the path for HTTP types.
@@ -61,13 +67,39 @@ type Route struct {
 	// Function defines the Function to execute when this route is called.
 	Function string `json:"function"`
 
-	// Frequency is used to define the frequency of scheduled_task routes in seconds.
+	// Frequency is used for both scheduled_task and init routes, with the value being the interval in seconds.
+	//
+	// When defined as an scheduled_task route, frequency is the interval at which tasks are executed.
+	// If no frequency is defined, an error will occur.
+	//
+	// When defined as an init route, frequency is used to define the interval in seconds between retries.
+	// As the number of retries increases, the interval between retries will increase exponentially.
+	// If no frequency is defined, the default value is 1 second.
 	Frequency int `json:"frequency,omitempty"`
+
+	// Retries is used to define the number of retries for init routes.
+	// If the init route fails, it will be retried for the number of times defined with a exponential backoff.
+	// The default value is 0 which means no retries.
+	Retries int `json:"retries,omitempty"`
 }
 
-// ErrRouteNotFound is returned when a requested route is not found in the
-// service's route configuration.
-var ErrRouteNotFound = fmt.Errorf("route not found")
+var (
+	// ErrRouteNotFound is returned when a requested route is not found in the
+	// service's route configuration.
+	ErrRouteNotFound = fmt.Errorf("route not found")
+
+	// ErrInvalidConfig is returned when the configuration file does not contain the required fields or is otherwise
+	// invalid.
+	ErrInvalidConfig = fmt.Errorf("invalid configuration file")
+)
+
+const (
+	// DefaultPoolSize is the default number of instances of a function to create.
+	DefaultPoolSize = 100
+
+	// DefaultFrequency is the default frequency for init routes.
+	DefaultFrequency = 1
+)
 
 // Parse function reads the file specified and attempts to parse the contents into a Config instance.
 func Parse(filepath string) (*Config, error) {
@@ -87,6 +119,11 @@ func Parse(filepath string) (*Config, error) {
 		return &Config{}, fmt.Errorf("could not parse service configuration file: %w", err)
 	}
 
+	// Validate the configuration
+	if err := cfg.Validate(); err != nil {
+		return &Config{}, ErrInvalidConfig
+	}
+
 	// Populate the internal routes map
 	cfg.routes = make(map[string]string)
 	for _, svcCfg := range cfg.Services {
@@ -101,6 +138,56 @@ func Parse(filepath string) (*Config, error) {
 	}
 
 	return cfg, nil
+}
+
+// Validate function checks the configuration for required fields and returns an error if any are missing.
+// Validate will also populate any default values for fields that are not defined.
+func (cfg *Config) Validate() error {
+	cfg.Lock()
+	defer cfg.Unlock()
+
+	// Loop through each service and validate the configuration
+	for sk, svcCfg := range cfg.Services {
+		// Validate the service name
+		if svcCfg.Name == "" {
+			return fmt.Errorf("service missing name: %w", ErrInvalidConfig)
+		}
+
+		// Validate functions
+		for fk, f := range svcCfg.Functions {
+			if f.Filepath == "" {
+				return fmt.Errorf("function missing filepath: %w", ErrInvalidConfig)
+			}
+			if f.PoolSize == 0 {
+				f.PoolSize = DefaultPoolSize
+				cfg.Services[sk].Functions[fk] = f
+			}
+		}
+
+		// Validate routes
+		for rk, r := range svcCfg.Routes {
+			if r.Type == "" || r.Function == "" {
+				return fmt.Errorf("route missing type or function: %w", ErrInvalidConfig)
+			}
+
+			// Validate the route type
+			switch r.Type {
+			case "http":
+				if r.Path == "" || len(r.Methods) == 0 {
+					return fmt.Errorf("http route missing path or methods: %w", ErrInvalidConfig)
+				}
+			case "scheduled_task":
+				if r.Frequency == 0 {
+					return fmt.Errorf("scheduled_task route missing frequency: %w", ErrInvalidConfig)
+				}
+			case "init":
+				if r.Frequency == 0 {
+					cfg.Services[sk].Routes[rk].Frequency = DefaultFrequency
+				}
+			}
+		}
+	}
+	return nil
 }
 
 // RouteLookup searches the routes map for a given key and returns the corresponding function name if the key is found,
