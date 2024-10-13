36bd71b80eefa802b4667a10b7943fab6fa7d812
Benjamin Cane <ben@bencane.com>
2024-10-13 08:26:05 -0700

    feat(sql): upgrade dependencies and enhance logging
    Upgraded TinyGo version and enhanced SQL handling to improve
    stability. Logging now adds a touch of empathy, so errors feel
    warm and cozy.

--- testdata/base/sql/main.go
+++ testdata/base/sql/main.go
@@ -5,6 +5,10 @@ import (
 	"fmt"
 
 	"github.com/tarmac-project/tarmac/pkg/sdk"
+	wapc "github.com/wapc/wapc-guest-tinygo"
+
+	"github.com/tarmac-project/protobuf-go/sdk/sql"
+	pb "google.golang.org/protobuf/proto"
 )
 
 var tarmac *sdk.Tarmac
@@ -20,11 +24,33 @@ func main() {
 }
 
 func Handler(payload []byte) ([]byte, error) {
-	// SQL Query
-	_, err := tarmac.SQL.Query(`CREATE TABLE IF NOT EXISTS wasmguest ( id int NOT NULL, name varchar(255), PRIMARY KEY (id) );`)
+	// Create SQL Request
+	query := &sql.SQLQuery{Query: []byte(`CREATE TABLE IF NOT EXISTS wasmguest ( id int NOT NULL, name varchar(255), PRIMARY KEY (id) );`)}
+	q, err := pb.Marshal(query)
 	if err != nil {
-		tarmac.Logger.Error(fmt.Sprintf("Unable to execute SQL query - %s", err))
-		return []byte(""), fmt.Errorf(`Failed to SQL callback - %s`, err)
+		tarmac.Logger.Error(fmt.Sprintf("Unable to marshal SQL query - %s", err))
+		return []byte(""), fmt.Errorf(`Failed to marshal SQL query - %s`, err)
+	}
+
+	// Call the SQL capability
+	rsp, err := wapc.HostCall("tarmac", "sql", "query", q)
+	if err != nil {
+		tarmac.Logger.Error(fmt.Sprintf("Unable to call SQL capability - %s", err))
+		return []byte(""), fmt.Errorf(`Failed to call SQL capability - %s`, err)
+	}
+
+	// Unmarshal the response
+	var response sql.SQLQueryResponse
+	err = pb.Unmarshal(rsp, &response)
+	if err != nil {
+		tarmac.Logger.Error(fmt.Sprintf("Unable to unmarshal SQL response - %s", err))
+		return []byte(""), fmt.Errorf(`Failed to unmarshal SQL response - %s`, err)
+	}
+
+	// Validate the response
+	if response.Status.Code != 200 {
+		tarmac.Logger.Error(fmt.Sprintf("SQL query failed - %s", response.Status.Status))
+		return []byte(""), fmt.Errorf(`SQL query failed - %s`, response.Status.Status)
 	}
 
 	// Return a happy message
